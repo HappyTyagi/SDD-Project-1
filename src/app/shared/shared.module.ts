e5b1a400d6a0e3955f22d83fb898805f8b4ec7a1
priti <priti.kumari@primussoft.com>
2021-10-05 12:22:01 +0530

    add header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [LoaderComponent],
-  exports: [LoaderComponent],
+  declarations: [LoaderComponent, HeaderComponent, SidebarComponent],
+  exports: [LoaderComponent, HeaderComponent, SidebarComponent],
   imports: [
     CommonModule
   ]
